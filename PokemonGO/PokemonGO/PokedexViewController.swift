8af33b04146e40ef45af1fb4b9e1806031343c5a
Brian pareja <brian.pareja@tecsup.edu.pe>
2017-06-05 16:51:44 -0500

    hasta mostrar todos los pokemons en el tableview

---
+++ PokemonGO/PokemonGO/PokedexViewController.swift
@@ -0,0 +1,64 @@
+//
+//  PokedexViewController.swift
+//  PokemonGO
+//
+//  Created by Brian Benjamin Pareja Meruvia on 5/06/17.
+//  Copyright © 2017 Brian Benjamin Pareja Meruvia. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var pokemonsAtrapados:[Pokemon] = []
+    var pokemonsNoAtrapados:[Pokemon] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        pokemonsAtrapados = obtenerPokemonsAtrapados()
+        pokemonsNoAtrapados = obtenerPokemonsNoAtrapados()
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return pokemonsAtrapados.count
+        }
+        else {
+            return pokemonsNoAtrapados.count
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pokemon : Pokemon
+        if indexPath.section == 0 {
+            pokemon = pokemonsAtrapados[indexPath.row]
+        }
+        else{
+            pokemon = pokemonsNoAtrapados[indexPath.row]
+        }
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.nombre
+        cell.imageView?.image = UIImage(named: pokemon.imagenNombre!)
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "Atrapados"
+            
+        }
+        else{
+            return "No atrapados"
+        }
+    }
+    
+    @IBAction func mapTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+}
--- PokemonGO/PokemonGO/ViewController.swift
+++ PokemonGO/PokemonGO/ViewController.swift
@@ -15,14 +15,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     var ubicacion = CLLocationManager()
     var contActualizaciones = 0
+    var pokemons:[Pokemon] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         ubicacion.delegate = self
+        pokemons = obtenerPokemons()
         
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
             mapView.showsUserLocation = true
             ubicacion.startUpdatingLocation()
+            Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
+                if let coord = self.ubicacion.location?.coordinate{
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = coord
+                    let randomLat = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    let randomLon = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    pin.coordinate.longitude += randomLon
+                    pin.coordinate.latitude += randomLat
+                    self.mapView.addAnnotation(pin)
+                }
+            })
         }
         else{
             ubicacion.requestWhenInUseAuthorization()
